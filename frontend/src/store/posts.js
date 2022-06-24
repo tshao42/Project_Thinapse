2d05932889fee40296d90f3097bac2f15286cf23
tshao <tshao0602@gmail.com>
2022-06-23 20:01:54 -0700

    first commit postCRUD

---
+++ frontend/src/store/posts.js
@@ -0,0 +1,4 @@
+export const LOAD_POSTS = "posts/LOAD_POSTS";
+export const UPDATE_POST = "posts/UPDATE_POST";
+export const REMOVE_POST = 'posts/REMOVE_POST';
+export const CREATE_POST = 'posts/CREATE_POST';
