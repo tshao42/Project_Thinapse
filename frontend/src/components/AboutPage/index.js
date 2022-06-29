088973eddc23e692ca5452ec3afcbd1d8ae36553
Tianyi Shao <tshao0602@gmail.com>
2022-06-29 08:40:22 -0700

    first version

--- frontend/src/components/AboutPage/index.js
+++ frontend/src/components/AboutPage/index.js
@@ -5,21 +5,30 @@ import './AboutPage.css'
 function AboutInfo(){
     return(
         <div class='postContainer'>
-        <h1>ThoughtBubble Info</h1>
+        <h1>About this application</h1>
         <div>A Medium imitation project with minimalist interface</div>
-        <ul class='textBody'>
+        <div></div>
+        <div class='textBody'>
             <div>Stack Used:</div>
             <ol>
                 <div>Backend: Express.js, Sequelize </div>
-                <div>Frontend: React.js, React-Redix</div>
+                <div>Frontend: React.js, Redux</div>
             </ol>
+                <div className='deviconsContainer'>
+                    <img className='devicons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img className='devicons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+                    <img className='devicons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-plain.svg" />
+                    <img className='devicons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                    <img className='devicons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+                    <img className='devicons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+                </div>
             <div>Github Repository:
                     <a href='https://github.com/tshao42/ThoughtBubble_React_Redux' style= {{textDecoration:'none', color:'#0077B6'}}>{`   `}ThoughtBubble React/Redux</a>
             </div>
             <div>LinkedIn Profile:
                 <a href='https://www.linkedin.com/in/tianyi-s-9345b213a/' style= {{textDecoration:'none', color:'#0077B6'}}>{`   `} My LinkedIn Profile</a>
             </div>
-        </ul>
+        </div>
         </div>
     )
 }
